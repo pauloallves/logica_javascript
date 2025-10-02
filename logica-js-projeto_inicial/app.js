5a899de1030c6214b80aad14daaa470d9102f98b
Paulo Henrique <paulohenriquesantos280@gmail.com>
2025-10-02 15:16:27 -0300

    quarto commit

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,7 +1,21 @@
-alert('Bem vindo ao jogo do número secreto');
+/*alert('Bem vindo ao jogo do número secreto');
 let numeroSecreto = 29;
 let chute = prompt ('Digite umm número entre 1 e 30');
 
 if(chute == numeroSecreto){
     console.log('Você acertou o número secreto!');
+}*/
+alert ("Bem vindo ao anosso site!");
+let nome = "Lua";
+let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 100;
+let mensagemDeErro = "Erro! Preencha todos os campos";
+alert (mensagemDeErro);
+let novoNome = prompt("Digite seu nome");
+let novaIdade = prompt("Digite sua idade");
+if (idade >= 18){
+    alert("Pode tirar a habilitação")
+}else{
+    alert("Você não possuí idade mínima para tirar a CNH")
 }
