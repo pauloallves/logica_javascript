18e978188f03133ab8c840d4505c4d0cbc8f9569
Paulo Henrique <paulohenriquesantos280@gmail.com>
2025-10-02 11:48:49 -0300

    terceiro commit

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,3 +1,7 @@
 alert('Bem vindo ao jogo do número secreto');
 let numeroSecreto = 29;
-prompt ('Digite umm número entre 1 e 30');
+let chute = prompt ('Digite umm número entre 1 e 30');
+
+if(chute == numeroSecreto){
+    console.log('Você acertou o número secreto!');
+}
