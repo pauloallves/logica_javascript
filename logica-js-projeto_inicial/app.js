cd0d28f169e906a13c016f6e7f4b98c938b71f58
Paulo Henrique <paulohenriquesantos280@gmail.com>
2025-10-03 08:57:05 -0300

    quinto commit

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -14,8 +14,12 @@ let mensagemDeErro = "Erro! Preencha todos os campos";
 alert (mensagemDeErro);
 let novoNome = prompt("Digite seu nome");
 let novaIdade = prompt("Digite sua idade");
-if (idade >= 18){
+let campo = "";
+if (novaIdade >= 18){
     alert("Pode tirar a habilitação")
-}else{
+}else if (novaIdade === ""){
+    alert("Você não informou sua idade")
+}
+else{
     alert("Você não possuí idade mínima para tirar a CNH")
 }
